a824d8d8bad3506fd24413b9f99084baa7ce8f5b
Inés Nieto <insherondale26@gmail.com>
2022-10-16 20:22:59 +0000

    TFG-22 Add calendar dialog (in progress)

--- src/app/my-angular-scheduler/my-angular-scheduler.component.ts
+++ src/app/my-angular-scheduler/my-angular-scheduler.component.ts
@@ -13,6 +13,11 @@ DragAndDropService} from '@syncfusion/ej2-angular-schedule';
 import { ChangeEventArgs } from '@syncfusion/ej2-calendars';
 import {FormControl} from "@angular/forms";
 import {AppointmentService} from "../../services/appointment.service";
+import {UserService} from "../../services/user.service";
+import {PsychologistInterface} from "../../interfaces/user-interface";
+import {cookieServiceFactory} from "ngx-cookie";
+import {finalize, map, Observable, startWith, switchMap, tap} from "rxjs";
+import {User} from "../../models/user";
 
 declare var require: any;
 
@@ -45,9 +50,10 @@ L10n.load({
   templateUrl: './my-angular-scheduler.component.html',
   styleUrls: ['./my-angular-scheduler.component.scss']
 })
-export class MyAngularSchedulerComponent implements OnInit {
-  public workWeekDays: number[] = [1, 2,3,4,5,6,7];
-
+export class MyAngularSchedulerComponent implements OnInit{
+  public workWeekDays: number[] = [1, 2,3,4,5];
+  today = new Date()
+  public minDate = new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate());
   public myselectedDate:Date = new Date(Date.now())
 
   @ViewChild('scheduleObj') calendar!: ScheduleComponent;
@@ -57,7 +63,10 @@ export class MyAngularSchedulerComponent implements OnInit {
 
   public setViews: View[] = ['Day', 'Month', 'WorkWeek']
 
-  public control = new FormControl('');
+  public psychologistControl = new FormControl('')
+  public startTimeControl = new FormControl('')
+  public endTimeControl = new FormControl('')
+
   public patient: string = '';
   public samplePatients = [
     {
@@ -70,21 +79,50 @@ export class MyAngularSchedulerComponent implements OnInit {
 
   public eventSettings?: EventSettingsModel
 
-  constructor(private http: HttpClient,
-              public appointmentService: AppointmentService) {
+  availableHours: Date[] = []
 
-  }
+  psychologistList!: User[]
+  availableStartAppointments!:Date[]
+  availableEndAppointments!:Date[]
+
+  selectedStartTime!:Date
+  selectedEndTime!:Date
+  selectedPsychologist!:string
+
+  constructor(private http: HttpClient, public appointmentService: AppointmentService,
+              public userService: UserService) {}
 
 
 
   ngOnInit(): void {
+    //TODO Aqui segun el rol del usuario se obtendran todos los psicologos, el psicologo que es o el psicologo del paciente
+    //Get psychologist list
+    this.userService.getPsychologists().subscribe((psychologists) => {
+      this.psychologistList = psychologists
+    })
+
+    //Get all appointments according to a role
     this.appointmentService.getAppointments().subscribe((response: any) => {
       let records: Record<string, any>[] = []
-
-      response.forEach((appointment:any) => records.push(appointment));
-      console.log(response)
-      this.eventSettings =  {dataSource:records }
+      response.forEach((appointment: any) => records.push(appointment));
+      this.eventSettings = {dataSource: records}
     })
+
+    this.psychologistControl.valueChanges.pipe(
+        startWith(this.psychologistControl.value),
+        switchMap((psychologist)=>
+          //Get available appointments from selected psychologist in the chosen day
+         this. getAvailableStartHours(psychologist).pipe()
+        )
+    ).subscribe((availableHours)=>{
+      if(this.psychologistControl.value){
+        this.availableStartAppointments = availableHours
+      }
+    })
+
+    this.startTimeControl.valueChanges.pipe(
+        startWith(this.startTimeControl.value)
+    )
   }
 
   // @ts-ignore
@@ -123,8 +161,14 @@ export class MyAngularSchedulerComponent implements OnInit {
     }
   }
 
-  public onPopupOpen(args: { type: string; data: { Patient: any; }; }) {
-      console.log(args)
+  public onPopupOpen(args: { type: string; data: { Patient: any, StartTime: Date }; }) {
+    //Adjust so the value is the correspondent
+    //let fecha = String(args.data.StartTime)
+    this.startTimeControl.setValue(args.data.StartTime.toLocaleTimeString())
+    this.psychologistControl.setValue(String(this.psychologistList[0]._id))
+    this.handleStartTimeClick(args.data.StartTime)
+    //this.selectedStartTime = args.data.StartTime
+
     if (args.type === 'Editor') {
       //Args data son los datos del evento
       //this.control.setValue(args.data.Patient);
@@ -132,7 +176,6 @@ export class MyAngularSchedulerComponent implements OnInit {
   }
 
   public onPopupClose(args:any) {
-      console.log(args)
     if (args.type === 'Editor' && args.data) {
       //args.data.Patient = this.patient;
     }
@@ -150,6 +193,42 @@ export class MyAngularSchedulerComponent implements OnInit {
 
   onDragStart(args: DragEventArgs): void {
       args.interval = 10; // drag interval time is changed to 10 minutes
+  }
+
+  getAvailableStartHours(psychologist:string |null): Observable<Array<Date>>{
+    return this.appointmentService.getAvailableStartAppointments(psychologist)
+  }
+
+  getAvailableEndHours(psychologist:string |null,chosenStart:Date): Observable<Array<Date>>{
+    return this.appointmentService.getAvailableEndAppointments(psychologist,chosenStart)
+  }
+
+  handleStartTimeClick(startTime:Date){
+    this.selectedStartTime = startTime
+
+    this.getAvailableEndHours(this.psychologistControl.value,this.selectedStartTime).subscribe((availableEndTimes)=>{
+      this.availableEndAppointments = availableEndTimes
+    })
+  }
+
+  handleEndTimeClick(endTime:Date){
+    this.selectedEndTime = endTime
+  }
+
+  displayFn(value?: number):string {
+
+    if(value){
+      let name = this.psychologistList.find(psychologist => psychologist._id === value)!.name
+      let surname = this.psychologistList.find(psychologist => psychologist._id === value)!.surname
+
+      return name+" "+surname
+    }else{
+      return ""
+    }
+  }
+
+  display(date:Date|null){
+    return date?.toLocaleTimeString()
   }
 
 }
--- src/services/appointment.service.ts
+++ src/services/appointment.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import {CanActivate, Router} from "@angular/router";
+import {DeserializeArray, IJsonArray} from "dcerialize";
+import {map, Observable} from "rxjs";
+import {User} from "../models/user";
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +16,17 @@ export class AppointmentService {
 
   getAppointments(): any{
     return this.http.get('/api/appointment')
+  }
+
+  getAvailableStartAppointments(id_psychologist:string|null): Observable<Array<Date>> {
+    let dayPsychologist = {day:'2022-10-19',id_psychologist:id_psychologist}
+    return (this.http.post<IJsonArray>('/api/available-start-appointment',dayPsychologist).pipe(
+        map((availableAppointments) => DeserializeArray(availableAppointments, () => Date))))
+  }
+
+  getAvailableEndAppointments(id_psychologist:string |null, chosenStart:Date): Observable<Array<Date>> {
+    let dayPsychologist = {day:'2022-10-19',id_psychologist:id_psychologist,chosen_start:chosenStart}
+    return (this.http.post<IJsonArray>('/api/available-end-appointment',dayPsychologist).pipe(
+        map((availableAppointments) => DeserializeArray(availableAppointments, () => Date))))
   }
 }
